91cf7d140dd8988fd935f34e67241692545738c7
Wisdom Ogwu <iammadab@gmail.com>
2026-02-05 14:34:21 +0100

    wip

--- src/engine/search/mod.rs
+++ src/engine/search/mod.rs
@@ -2,9 +2,9 @@ pub mod alphabeta;
 pub mod minimax;
 pub mod traits;
 
-#[cfg(test)]
-mod tests;
-
 pub use alphabeta::AlphaBetaSearch;
 pub use minimax::MinimaxSearch;
 pub use traits::{SearchAlgorithm, SearchResult};
+
+#[cfg(test)]
+mod tests;
